3350378a110c007e7ffebf951ca8832878a6f656
Sam Richard <snugug@gmail.com>
2013-03-12 11:04:59 -0400

    Formalize projcet

---
+++ templates/formalize/manifest.rb
@@ -0,0 +1,26 @@
+description "Formalize for Normalize"
+
+discover :javascripts
+
+image button.png, :like => :image, :to => 'formalize/button.png'
+image select_arrow.png, :like => :image, :to => 'formalize/select_arrow.png'
+
+help %Q{
+  Please see the normalize.css website for documentation and more information
+
+        http://necolas.github.com/normalize.css/
+
+  For more information on how normalize.css works please visit the wiki
+
+        https://github.com/necolas/normalize.css/wiki
+}
+
+welcome_message %Q{
+  Please see the normalize.css website for documentation and more information
+
+        http://necolas.github.com/normalize.css/
+
+  For more information on how normalize.css works please visit the wiki
+
+        https://github.com/necolas/normalize.css/wiki
+}
